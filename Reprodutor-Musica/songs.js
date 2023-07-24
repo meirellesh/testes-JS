b02ff841a124e467e09c732c92c7a610d93fbada
Henrique <meirelles.henrique@outlook.com>
2023-07-24 18:00:26 -0300

    Teste Cronometro/Reprodutor de Música

---
+++ Reprodutor-Musica/songs.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        src: "./src/SnapInsta.io - 5 Minute Timer Lofi (128 kbps).mp3",
+        name: "Lofi 5 minutos - 1",
+    },
+    {
+        src: "./src/SnapInsta.io - 5 Minute Timer Relaxing Music Lofi Fish Background (128 kbps).mp3",
+        name: "Lofi 5 minutos - 2",
+    },
+    {
+        src: "./src/SnapInsta.io - 5 MINUTES OF (No Copyright Music) CHILL LOFI HIP HOP BEAT (Royalty free) (320 kbps).mp3",
+        name: "Lofi 5 minutos - 3"
+    }
+];
